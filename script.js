4a778befc78938e65e4a8ddd6689a833e1d2e5fb
Walliston <tontongamer111@gmail.com>
2024-10-18 15:33:23 -0300

    primeiro loop

---
+++ script.js
@@ -0,0 +1,24 @@
+// Loops
+
+let numero = 1;
+// number
+// os numeros são dados do tipo number
+
+console.log(numero);
+console.log(numero + 1);
+
+// for
+// estrutura for
+// for(varieval inicial; condição;iteração){}
+// ++ = +1
+// -- = -1
+for(let i = 0;i <= 5; i++){
+    console.log(i)
+}
+
+// while
+let contador = 0;
+while (contador < 12){
+    contador++;
+    console.log(contador)
+}
